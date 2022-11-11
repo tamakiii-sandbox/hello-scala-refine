8ac436f8d83d86086daa7179576ac2316fb266cd
tamakiii <tamakiii@users.noreply.github.com>
2022-11-11 20:23:34 +0900

    using refined

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,14 @@
-@main def hello: Unit = 
-  println("Hello world!")
-  println(msg)
+import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric.Positive
 
-def msg = "I was compiled by Scala 3. :)"
+object Main extends App {
+  val i1: Int Refined Positive = 5
+  println("Hello, World!")
+}
+
+// @main def hello: Unit = 
+//   println("Hello world!")
+//   println(msg)
+//
+// def msg = "I was compiled by Scala 3. :)"
