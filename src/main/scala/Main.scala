74ae30fc9ea7d6d47fe9510a836cd74c695a469b
tamakiii <tamakiii@users.noreply.github.com>
2022-11-12 00:04:23 +0900

    error found

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,13 @@ import eu.timepit.refined.numeric.Positive
 
 object Main extends App {
   val i1: Int Refined Positive = 5
+  // val i2: Int Refined Positive = -5
+  // // [error] /work/src/main/scala/Main.scala:7:34: Predicate failed: (-5 > 0).
+  // // [error]   val i2: Int Refined Positive = -5
+  // // [error]                                  ^
+  // // [error] one error found
+  // // [error] (Compile / compileIncremental) Compilation failed
+  // // [error] Total time: 3 s, completed Nov 11, 2022, 11:49:02 AM
   println("Hello, World!")
 }
 
