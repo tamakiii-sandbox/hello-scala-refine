be74997765ccb0c370787357d7c98f040f67842b
tamakiii <tamakiii@users.noreply.github.com>
2022-11-11 15:01:26 +0900

    add bash rule

--- docker.mk
+++ docker.mk
@@ -1,12 +1,15 @@
-.PHONY: help build clean
+.PHONY: help build bash clean
 
 IMAGE := tamakiii-sandbox/hello-scala-refine
 
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
-build:
+build: Dockerfile
 	docker build -t $(IMAGE) .
+
+bash: build
+	docker run --rm -it $(IMAGE) $@
 
 clean:
 	docker image rm $(IMAGE)
